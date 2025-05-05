a5b2e865c6227839816ff7a910f7dd6852e7fdf5
krisrazumeiko <106400261+krisrazumeiko@users.noreply.github.com>
2025-05-05 21:30:27 +0300

    Add files via upload

---
+++ hw_2_2_4.java
@@ -0,0 +1,19 @@
+//4. Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b, и инициализируйте их любыми значениями,
+//  которыми захотите. Если a больше или равно b, то необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”;
+
+public class hw_2_2_4 {
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+
+    public static void compareNumbers() {
+        int a = 1;
+        int b = 3;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}
